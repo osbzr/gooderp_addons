033f3e51f481edd8810025b17a021184693f3905
Zxy <judy@osbzr.com>
2016-04-01 14:00:27 +0800

    [FIX]money_order,当单据为完成状态时，不能删除。完善期初余额表

--- money/go_live_order.py
+++ money/go_live_order.py
@@ -7,13 +7,24 @@ class go_live_order(models.Model):
     _description = u'期初余额表'
 
     @api.model
+    def create(self, values):
+        new_id = super(go_live_order, self).create(values)
+        if values.get('bank_id'):
+            self.env['bank.account'].search([('id', '=', values.get('bank_id'))]).balance += values.get('balance')
+        if values.get('partner_id'):
+            self.env['partner'].search([('id', '=', values.get('partner_id'))]).receivable += values.get('receivable')
+            self.env['partner'].search([('id', '=', values.get('partner_id'))]).payable += values.get('payable')
+
+        return new_id
+
+    @api.model
     def _get_company_start_date(self):
         return self.env.user.company_id.start_date
 
     partner_id = fields.Many2one('partner', string=u'业务伙伴')
     bank_id = fields.Many2one('bank.account', string=u'账户')
     name = fields.Char(string=u'编号', copy=False, readonly=True, default='/')
-    date = fields.Date(string=u'日期', default=_get_company_start_date)
+    date = fields.Date(string=u'日期', default=_get_company_start_date, readonly=True, required=True)
     receivable = fields.Float(u'应收余额')
     payable = fields.Float(u'应付余额')
     balance = fields.Float(u'期初余额')
