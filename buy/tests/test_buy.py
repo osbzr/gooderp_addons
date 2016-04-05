7643fa05edfbd4c3486b810f9a0e96f89e765380
jason <jason@osbzr.com>
2016-04-05 18:01:58 +0800

    [add]test_buy的测试脚本。

--- buy/tests/test_buy.py
+++ buy/tests/test_buy.py
@@ -7,6 +7,27 @@ class test_buy(TransactionCase):
         ''' 测试采购订单  '''
         order = self.env.ref('buy.buy_order_1')
         
+        #采购订单行的已入库数量为0时，将产品状态写为未入库
+        new_order = order.copy()
+        new_order._get_buy_goods_state()
+        #采购订单行的已入库数量小于产品数量时，将产品状态写为部分入库
+        for line in new_order.line_ids :
+            line.quantity_in = 5
+        new_order._get_buy_goods_state()
+            
+        #产品使用批次管理时，将订单行拆分
+        new_order = order.copy()
+        for line in new_order.line_ids :
+            line.goods_id = 1
+            new_order.buy_generate_receipt()
+        #订单行新建时，调出仓库的默认值？
+            line.create({
+                         'order_id':new_order.id,})
+        #采购订单行为空时，不能审核
+            line.unlink()
+        with self.assertRaises(except_orm):
+            new_order.buy_order_done()
+        
         # 审核采购订单
         order.buy_order_done()
         order.buy_order_draft()
