3791449e965b8626a9b67adb452db2b74a1a2db2
Zxy <judy@osbzr.com>
2016-04-08 14:21:40 +0800

    [ADD]money: 添加业务伙伴对账单查看带商品明细对账单的测试数据

--- sell/tests/test_report.py
+++ sell/tests/test_report.py
@@ -28,9 +28,36 @@ class test_report(TransactionCase):
         
         # 执行向导，正常输出
         # 执行 if条件self._context.get('default_customer')
+        self.env.ref('warehouse.wh_move_line_14').action_done()
+        order = self.env['money.order'].create({'name': 'GET20160001',
+                                          'partner_id': self.env.ref('core.jd').id,
+                                          'date': '2016-04-07',
+                                          'line_ids': [(0, 0, {'bank_id': self.env.ref('core.comm').id, 'amount': 400})],
+                                          'type': 'get'
+                                          })
+        order.money_order_done()
+        sell_order = self.env['sell.order'].create({'name': 'SELL20160001',
+                                               'date': '2016-04-07',
+                                               'partner_id': self.env.ref('core.jd').id,
+                                               'line_ids': [(0, 0, {
+                                                               'goods_id': self.env.ref('goods.cable').id,
+                                                               'uom_id': self.env.ref('core.uom_pc').id,
+                                                               'warehouse_id': self.env.ref('warehouse.hd_stock').id,
+                                                               'warehouse_dest_id': self.env.ref('warehouse.warehouse_customer').id,
+                                                               'quantity': 1,
+                                                               'price': 60.0,
+                                                               'discount_amount': 60.0})],
+                                               'type': 'sell',
+                                          })
+        sell_order.sell_order_done()
+        receipt = self.env['sell.delivery'].search([('order_id','=',sell_order.id)])
+        receipt.sell_delivery_done()
+        invoice = self.env['money.invoice'].search([('name','=',receipt.name)])
+        invoice.money_invoice_done()
         statement = self.env['partner.statements.report.wizard'].create(
-                    {'partner_id': 
-                     self.env.ref('sell.sell_order_1').partner_id.id}).with_context({'default_customer': True})
+                    {'partner_id': order.partner_id.id,
+                    'from_date': '2016-01-01',
+                    'to_date': '2016-11-01'}).with_context({'default_customer': True})
         # 输出报表，正常输出
         statement.partner_statements_without_goods()
         statement.partner_statements_with_goods()
