9208aa26268a9e688f84e76c06fbb2d53ad07ef6
OpenERPJeff <jeff@osbzr.com>
2016-04-09 00:07:51 +0800

    [ADD]菜单新建按钮的测试用例

---
+++ web_menu_create/tests/test_menu.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from openerp.tests.common import TransactionCase
+
+
+class test_menu(TransactionCase):
+    ''' 测试菜单 '''
+    def test_tag(self):
+       '''测试菜单'''
+       a = self.browse_ref('base.menu_ir_property').load_create_tag()
