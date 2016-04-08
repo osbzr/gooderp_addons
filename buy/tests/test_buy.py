9d669852a6d8d8a517e810703713b59e8c9dd88b
jason <jason@osbzr.com>
2016-04-08 15:01:47 +0800

    [add]buy.py增加test用例

--- buy/tests/test_buy.py
+++ buy/tests/test_buy.py
@@ -20,14 +20,11 @@ class test_buy(TransactionCase):
         for line in new_order.line_ids :
             line.goods_id = 1
             new_order.buy_generate_receipt()
-        #订单行新建时，调出仓库的默认值？
-            line.create({
-                         'order_id':new_order.id,})
         #采购订单行为空时，不能审核
             line.unlink()
         with self.assertRaises(except_orm):
             new_order.buy_order_done()
-        
+            
         # 审核采购订单
         order.buy_order_done()
         order.buy_order_draft()
@@ -39,10 +36,62 @@ class test_buy(TransactionCase):
         # 已经收货不能反审核了
         with self.assertRaises(except_orm):
             order.buy_order_draft()
+        
+        # 入库单测试(_get_buy_money_state)
+        new_order = order.copy()
+        new_order.buy_order_done()
+        receipt = self.env['buy.receipt'].search([('order_id','=',order.id)])
+        receipt.write({'state':'draft'})
+        receipt._get_buy_money_state()
+        receipt.buy_receipt_done()
+        receipt._get_buy_money_state()
+        receipt.write({'payment':receipt.amount - 1})
+        receipt._get_buy_money_state()
+        receipt.write({'payment':receipt.amount})
+        receipt._get_buy_money_state()
+        #移库单审核
+        bank_account = self.env.ref('core.alipay')
+        bank_account.write({'balance':1000000,})
+        receipt.write({'state':'draft',
+                       'bank_account_id':bank_account.id,
+                       'payment':'',})
+        with self.assertRaises(except_orm):
+            receipt.buy_receipt_done()
+        receipt.write({'state':'draft',
+                       'bank_account_id':'',
+                       'payment':10,})
+        with self.assertRaises(except_orm):
+            receipt.buy_receipt_done()
+        receipt.write({'state':'draft',
+                       'bank_account_id':bank_account.id,
+                       'payment':receipt.amount + 1,})
+        with self.assertRaises(except_orm):
+            receipt.buy_receipt_done()
+        #采购费用行判断
+        receipt.write({'state':'draft',
+                       'bank_account_id':'',
+                       'payment':'',})
+        receipt.cost_line_ids.create({'buy_id':receipt.id,
+                          'partner_id':4,
+                          'amount':100,})
+        receipt.buy_receipt_done()
+        #测试生成付款单
+        receipt.write({'state':'draft',
+                       'bank_account_id':bank_account.id,
+                       'payment':receipt.amount,})
+        receipt.buy_receipt_done()
+        #移库单审核返回True
+        new_receipt = receipt.copy()
+        new_receipt.write({'state':'draft',
+                       'order_id':'',})
+        new_receipt.buy_receipt_done()
+        
 
     def test_onchange(self):
         ''' 测试onchange '''
         order = self.env.ref('buy.buy_order_1')
+        order.write({'discount_rate':10})
         order.onchange_discount_rate()
         order.line_ids[0].onchange_goods_id()
+        order.line_ids[0].write({'discount_rate':10})
         order.line_ids[0].onchange_discount_rate()
