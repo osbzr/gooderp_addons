e3ec67be23bd2af26d23e7c78fa370e6ee58cc91
jason <jason@osbzr.com>
2016-04-09 16:18:16 +0800

    [add]buy.py测试用例

--- buy/tests/test_buy.py
+++ buy/tests/test_buy.py
@@ -24,6 +24,11 @@ class test_buy(TransactionCase):
             line.unlink()
         with self.assertRaises(except_orm):
             new_order.buy_order_done()
+        #新建订单行，默认值
+        new_order = order.copy()
+        line_id = new_order.line_ids.with_context({'warehouse_type':'supplier'}).create({
+                                   'order_id':new_order.id,
+                                   })
             
         # 审核采购订单
         order.buy_order_done()
@@ -40,7 +45,7 @@ class test_buy(TransactionCase):
         # 入库单测试(_get_buy_money_state)
         new_order = order.copy()
         new_order.buy_order_done()
-        receipt = self.env['buy.receipt'].search([('order_id','=',order.id)])
+        receipt = self.env['buy.receipt'].search([('order_id','=',new_order.id)])
         receipt.write({'state':'draft'})
         receipt._get_buy_money_state()
         receipt.buy_receipt_done()
@@ -85,17 +90,64 @@ class test_buy(TransactionCase):
         new_receipt.write({'state':'draft',
                        'order_id':'',})
         new_receipt.buy_receipt_done()
+        with self.assertRaises(except_orm):
+            new_receipt.buy_receipt_done()
+        
+        #退货单测试
+        order_return = self.env.ref('buy.buy_order_return_1')
+        order_return_copy = order_return.copy()
+        return_line = self.env['wh.move.line'].create({
+                                          'move_id':order_return_copy.buy_move_id.id,
+                                          'goods_id':1,
+                                          'warehouse_dest_id':1,
+                                          'warehouse_id':2,
+                                          'goods_qty':1,
+                                          'price':5,
+                                          'type':'out',
+                                          })
+        order_return_copy._get_buy_return_state()
+        order_return_copy.buy_receipt_done()
+        order_return_copy._get_buy_return_state()
+        order_return_copy.write({
+                                 'payment':5,
+                                 })
+        order_return_copy._get_buy_return_state()
+        order_return_copy.write({
+                                 'payment':5.85,
+                                 })
+        order_return_copy._get_buy_return_state()
         
 
     def test_onchange(self):
         ''' 测试onchange '''
         order = self.env.ref('buy.buy_order_1')
+
+        new_order = order.copy()
+        new_order.buy_order_done()
+        receipt = self.env['buy.receipt'].search([('order_id','=',new_order.id)])
+        receipt.onchange_discount_rate()
+        receipt.write({'discount_rate':10})
+        receipt.onchange_discount_rate()
+        
+        order_return = self.env.ref('buy.buy_order_return_1')
+        order_return_copy = order_return.copy()
+        return_line = self.env['wh.move.line'].create({
+                                          'move_id':order_return_copy.buy_move_id.id,
+                                          'warehouse_dest_id':1,
+                                          'goods_id':1,
+                                          'warehouse_id':2,
+                                          'goods_qty':1,
+                                          'price':5,
+                                          'type':'out',
+                                          })
+        order_return_copy.onchange_discount_rate()
+        
         order.write({'discount_rate':10})
         order.onchange_discount_rate()
         order.line_ids[0].onchange_goods_id()
         order.line_ids[0].write({'discount_rate':10})
         order.line_ids[0].onchange_discount_rate()
-
+        
     def test_buy_done(self):
         ''' 测试审核采购订单  '''
         order = self.env.ref('buy.buy_order_1')
