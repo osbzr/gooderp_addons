bc1a95b42ac834d43600bd0a338eb732139437bf
朱正翔 <feiyuc8@gmail.com>
2016-04-14 16:00:24 +0800

    添加goods的测试案例

--- goods/tests/test_goods.py
+++ goods/tests/test_goods.py
@@ -11,3 +11,12 @@ class test_goods(TransactionCase):
         self.assertEqual(res1 , 120)
         res2 = self.env.ref('goods.cable').anti_conversion_unit(12)
         self.assertEqual(res2, 1)
+
+    def test_uom(self):
+        cable = self.browse_ref('goods.cable')
+        uom_pc = self.browse_ref('core.uom_pc')
+
+        cable.uom_id = uom_pc
+        cable.onchange_uom()
+
+        self.assertEqual(cable.uos_id, cable.uom_id)
