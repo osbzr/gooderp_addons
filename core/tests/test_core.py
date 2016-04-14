6d3bb144ce3c379506836e9a227eec539d71a12b
OpenERPJeff <jeff@osbzr.com>
2016-04-14 11:17:42 +0800

    [UPD]core 实现many2one已使用的记录不可删除

--- core/tests/test_core.py
+++ core/tests/test_core.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 from openerp.tests.common import TransactionCase
-from openerp.exceptions import except_orm
+from psycopg2 import IntegrityError
 
 
 class test_core(TransactionCase):
     
     def test_partner(self):
         ''' 测试删除已有客户的分类报错 '''
-        with self.assertRaises(except_orm):
+        with self.assertRaises(IntegrityError):
             self.env.ref('core.customer_category_1').unlink()
