07b5fd51d9b3117a62e1ee1e278356374267bfaf
jason <jason@osbzr.com>
2016-04-15 12:07:39 +0800

    [add]finance模块

---
+++ finance/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "GOODERP 会计模块",
+    'author': "开阖软件",
+    'website': "http://www.osbzr.com",
+    'category': 'gooderp',
+    "description": """
+    """,
+    'version': '8.0.0.1',
+    'depends': ['base', 'decimal_precision'],
+    'demo': [
+             'tests/finance_demo.xml',
+        ],
+    'data': [
+        'data/finance_data.xml',
+        'finance_conf.xml',
+        'finance_view.xml',
+        ],
+}
