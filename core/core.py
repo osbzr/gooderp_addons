cdd364ce9dbfd53512a7eff9b2403d3fe440e309
袁文浦 <gilbert@osbzr.com>
2016-04-18 16:44:58 +0800

    添加一个 把阿拉伯数字换成中文大写数字的widget

--- core/core.py
+++ core/core.py
@@ -85,7 +85,7 @@ class goods_price(models.Model):
     category_id = fields.Many2one('core.category', u'客户类别',
                                   ondelete='cascade',
                                   domain=[('type', '=', 'customer')],
-                                  context={'type':'customer'})
+                                  context={'type': 'customer'})
     price = fields.Float(u'价格')
 
 
@@ -103,3 +103,39 @@ class bank_account(models.Model):
     _name = 'bank.account'
     name = fields.Char(u'名称')
     balance = fields.Float(u'余额', readonly=True)
+
+
+class res_currency(models.Model):
+    _inherit = 'res.currency'
+
+    @api.model
+    def rmb_upper(self, value):
+        """
+        人民币大写
+        来自：http://topic.csdn.net/u/20091129/20/b778a93d-9f8f-4829-9297-d05b08a23f80.html
+        传入浮点类型的值返回 unicode 字符串
+        """
+        rmbmap = [u"零", u"壹", u"贰", u"叁", u"肆", u"伍", u"陆", u"柒", u"捌", u"玖"]
+        unit = [u"分", u"角", u"元", u"拾", u"佰", u"仟", u"万", u"拾", u"佰", u"仟", u"亿",
+                u"拾", u"佰", u"仟", u"万", u"拾", u"佰", u"仟", u"兆"]
+
+        nums = map(int, list(str('%0.2f' % value).replace('.', '')))
+        words = []
+        zflag = 0  # 标记连续0次数，以删除万字，或适时插入零字
+        start = len(nums) - 3
+        for i in range(start, -3, -1):  # 使i对应实际位数，负数为角分
+            if 0 != nums[start - i] or len(words) == 0:
+                if zflag:
+                    words.append(rmbmap[0])
+                    zflag = 0
+                words.append(rmbmap[nums[start - i]])
+                words.append(unit[i + 2])
+            elif 0 == i or (0 == i % 4 and zflag < 3):  # 控制‘万/元’
+                words.append(unit[i + 2])
+                zflag = 0
+            else:
+                zflag += 1
+
+        if words[-1] != unit[0]:  # 结尾非‘分’补整字
+            words.append(u"整")
+        return ''.join(words)
