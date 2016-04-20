4c42d77547f3a471d62bf3cbe7f968605caa2640
袁文浦 <gilbert@osbzr.com>
2016-04-20 16:15:44 +0800

    测试数据的错误修正

--- home_page/tests/test_home_page.py
+++ home_page/tests/test_home_page.py
@@ -2,10 +2,8 @@
 from openerp.tests.common import TransactionCase
 
 
-class test_invoice(TransactionCase):
-
-    def setUp(self):
-        pass
+class test_home_page(TransactionCase):
 
     def test_home_page(self):
+        """测试首页的显示情况"""
         self.env['home.page'].get_action_url()
