84c2e9f87030f7ab0f30e1fb063a4cde877b6392
JasonChen <jason@osbzr.com>
2016-04-25 16:13:48 +0800

    [add]会计凭证审核时如果期间已关闭则报错

--- finance/finance.py
+++ finance/finance.py
@@ -57,12 +57,16 @@ class voucher(models.Model):
     def voucher_done(self):
         if self.state == 'done':
             raise except_orm(u'错误', u'请不要重复审核！')
+        if self.period_id.is_closed == True:
+            raise except_orm(u'错误', u'该会计期间已结账！不能审核')
         self.state='done'
 
     @api.one
     def voucher_draft(self):
         if self.state == 'draft':
             raise except_orm(u'错误', u'请不要重复反审核！')
+        if self.period_id.is_closed == True:
+            raise except_orm(u'错误', u'该会计期间已结账！不能反审核')
         self.state='draft'
 
     @api.one
