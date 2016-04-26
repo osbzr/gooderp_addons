d78e5c821917651e7a26847825cf883a221429c2
OpenERPJeff <jeff@osbzr.com>
2016-04-27 00:47:11 +0800

    retry

--- finance/tests/test_report.py
+++ finance/tests/test_report.py
@@ -13,20 +13,21 @@ class test_report(TransactionCase):
         self.env.ref('finance.voucher_2').voucher_done()
         # 结转2015年12月的期间
         self.env['checkout.wizard'].create(
-                       {'date':'2015-12-31'}).button_checkout()      
+                       {'date':'2015-12-31'}).button_checkout()
+        self.period_id = self.env.ref('finance.period_201512').id
 
     def test_trail_balance(self):
         ''' 测试科目余额表 '''
         report = self.env['create.trial.balance.wizard'].create(
-            {'period_id': self.env.ref('finance.period_201512')}
+            {'period_id': self.period_id}
                     )
         report.create_trial_balance()
 
     def test_vouchers_summary(self):
         ''' 测试总账和明细账'''
         report = self.env['create.trial.balance.wizard'].create(
-            {'period_begin_id': self.env.ref('finance.period_201512'),
-             'period_end_id': self.env.ref('finance.period_201512'),
+            {'period_begin_id': self.period_id,
+             'period_end_id': self.period_id,
              'subject_name_id': self.env.ref('finance.account_fund')}
                     )
         report.create_vouchers_summary()
@@ -35,7 +36,7 @@ class test_report(TransactionCase):
     def test_balance_sheet(self):
         ''' 测试科目余额表 '''
         report = self.env['create.balance.sheet.wizard'].create(
-            {'period_id': self.env.ref('finance.period_201512')}
+            {'period_id': self.period_id}
                     )
         report.create_balance_sheet()
         report.create_profit_statement()
