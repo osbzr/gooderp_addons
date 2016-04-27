415164127a57e80a6f51efa189e771754be88e6c
JasonChen <jason@osbzr.com>
2016-04-27 17:31:03 +0800

    [add]finance测试用例

--- finance/tests/test_report.py
+++ finance/tests/test_report.py
@@ -50,3 +50,60 @@ class test_report(TransactionCase):
             report.create_balance_sheet()
         with self.assertRaises(except_orm):
             report.create_profit_statement()
+
+
+class test_checkout_wizard(TransactionCase):
+    
+    def setUp(self):
+        super(test_checkout_wizard, self).setUp()
+        self.voucher_15_12 = self.env.ref('finance.voucher_12')
+        self.checkout_voucher = self.env.ref('finance.voucher_12_1')
+        self.period_15_12 = self.env.ref('finance.period_201512')
+
+    def test_button_checkout(self):
+        '''结账按钮,及新建wizard时的onchange'''
+        checkout_obj = self.env['checkout.wizard']
+        wizard = checkout_obj.create({'date':'20160102'})
+        #onchange 拿到会计期间
+        wizard.onchange_period_id()
+        self.assertTrue(wizard.period_id.name == u'2016年 第1期')
+        #上期间未关闭报错
+        with self.assertRaises(except_orm):
+            wizard.button_checkout()
+        wizard.date = u'2015-12-31'
+        wizard.onchange_period_id()
+        #本期间已结账报错
+        self.period_15_12.is_closed = True
+        with self.assertRaises(except_orm):
+            wizard.button_checkout()
+        self.period_15_12.is_closed = False
+        #期间内凭证未审核报错
+        with self.assertRaises(except_orm):
+            wizard.button_checkout()
+        #正常流程
+        self.voucher_15_12.voucher_done()
+        self.checkout_voucher.voucher_done()
+        wizard.button_checkout()
+        #费用大于收入
+        wizard.button_counter_checkout()
+        self.env.ref('finance.voucher_12_2_debit').debit = 100000
+        self.env.ref('finance.voucher_12_2_credit').credit = 100000
+        wizard.button_checkout()
+        #检查下一个会计区间
+        self.env.ref('finance.voucher_1').voucher_done()
+        self.env.ref('finance.voucher_2').voucher_done()
+        self.env.ref('finance.voucher_4').voucher_done()
+        self.env.ref('finance.voucher_14_12').voucher_done()
+        wizard.date='2016-01-31'
+        wizard.onchange_period_id()
+        wizard.button_checkout()
+        wizard.date='2016-02-28'
+        wizard.onchange_period_id()
+        wizard.button_checkout()
+        wizard.date='2014-12-28'
+        wizard.onchange_period_id()
+        wizard.button_checkout()
+
+
+
+
--- finance/wizard/checkout_wizard.py
+++ finance/wizard/checkout_wizard.py
@@ -9,8 +9,8 @@ class checkout_wizard(models.TransientModel):
     '''月末结账的向导'''
     _name = 'checkout.wizard'
 
-    period_id = fields.Many2one('finance.period',u'结账会计期间',required=True)
-    date = fields.Date(u'生成凭证日期')
+    period_id = fields.Many2one('finance.period',u'结账会计期间')
+    date = fields.Date(u'生成凭证日期',required=True)
 
     @api.multi
     @api.onchange('date')
@@ -21,134 +21,136 @@ class checkout_wizard(models.TransientModel):
     def button_checkout(self):
         if self.period_id:
             last_period = self.env['create.trial.balance.wizard'].compute_last_period_id(self.period_id)
-            if not last_period.is_closed:
-                raise except_orm(u'错误', u'上一个期间%s未结账' % last_period.name)
+            if last_period:
+                if not last_period.is_closed:
+                    raise except_orm(u'错误', u'上一个期间%s未结账' % last_period.name)
             if self.period_id.is_closed:
                 raise except_orm(u'错误', u'本期间已结账')
-            voucher_obj = self.env['voucher']
-            voucher_ids = voucher_obj.search([('period_id','=',self.period_id.id)])
-            i=0
-            for voucher_id in voucher_ids:
-                if voucher_id.state != 'done':
-                    i += 1
-            if i != 0:
-                raise except_orm(u'错误', u'该期间有%s张凭证未审核' % i)
             else:
-                voucher_line = [] #生成的结账凭证行
-                account_obj = self.env['finance.account']
-                company_obj = self.env['res.company']
-                voucher_line_obj = self.env['voucher.line']
-                revenue_account_ids = account_obj.search([('costs_types','=','in')]) #收入类科目
-                expense_account_ids = account_obj.search([('costs_types','=','out')]) #费用类科目
-                revenue_total=0 #收入类科目合计
-                expense_total=0 #费用类科目合计
-                for revenue_account_id in revenue_account_ids:
-                    voucher_line_ids = voucher_line_obj.search([
-                                        ('account_id','=',revenue_account_id.id),
-                                        ('voucher_id.period_id','=',self.period_id.id)])
-                    credit_total = 0
-                    for voucher_line_id in voucher_line_ids:
-                        credit_total += voucher_line_id.credit
-                    revenue_total += credit_total
-                    if credit_total!= 0:
+                voucher_obj = self.env['voucher']
+                voucher_ids = voucher_obj.search([('period_id','=',self.period_id.id)])
+                i=0
+                for voucher_id in voucher_ids:
+                    if voucher_id.state != 'done':
+                        i += 1
+                if i != 0:
+                    raise except_orm(u'错误', u'该期间有%s张凭证未审核' % i)
+                else:
+                    voucher_line = [] #生成的结账凭证行
+                    account_obj = self.env['finance.account']
+                    company_obj = self.env['res.company']
+                    voucher_line_obj = self.env['voucher.line']
+                    revenue_account_ids = account_obj.search([('costs_types','=','in')]) #收入类科目
+                    expense_account_ids = account_obj.search([('costs_types','=','out')]) #费用类科目
+                    revenue_total=0 #收入类科目合计
+                    expense_total=0 #费用类科目合计
+                    for revenue_account_id in revenue_account_ids:
+                        voucher_line_ids = voucher_line_obj.search([
+                                            ('account_id','=',revenue_account_id.id),
+                                            ('voucher_id.period_id','=',self.period_id.id)])
+                        credit_total = 0
+                        for voucher_line_id in voucher_line_ids:
+                            credit_total += voucher_line_id.credit
+                        revenue_total += credit_total
+                        if credit_total!= 0:
+                            res={
+                                 'name':u'月末结账',
+                                 'account_id':revenue_account_id.id,
+                                 'debit':credit_total,
+                                 'credit':0,
+                                 }
+                            voucher_line.append(res)
+                    for expense_account_id in expense_account_ids:
+                        voucher_line_ids = voucher_line_obj.search([
+                                            ('account_id','=',expense_account_id.id),
+                                            ('voucher_id.period_id','=',self.period_id.id)])
+                        debit_total = 0
+                        for voucher_line_id in voucher_line_ids:
+                            debit_total += voucher_line_id.debit
+                        expense_total += debit_total
+                        if debit_total != 0:
+                            res={
+                                 'name':u'月末结账',
+                                 'account_id':expense_account_id.id,
+                                 'debit':0,
+                                 'credit':debit_total,
+                                 }
+                            voucher_line.append(res)
+                    #利润结余
+                    year_profit_account = company_obj.search([])[0].profit_account
+                    if (revenue_total - expense_total) > 0:
                         res={
-                             'name':u'月末结账',
-                             'account_id':revenue_account_id.id,
-                             'debit':credit_total,
+                             'name':u'利润结余',
+                             'account_id':year_profit_account.id,
+                             'debit':0,
+                             'credit':revenue_total - expense_total,
+                             }
+                        voucher_line.append(res)
+                    if (revenue_total - expense_total) < 0:
+                        res={
+                             'name':u'利润结余',
+                             'account_id':year_profit_account.id,
+                             'debit':expense_total - revenue_total,
                              'credit':0,
                              }
                         voucher_line.append(res)
-                for expense_account_id in expense_account_ids:
-                    voucher_line_ids = voucher_line_obj.search([
-                                        ('account_id','=',expense_account_id.id),
-                                        ('voucher_id.period_id','=',self.period_id.id)])
-                    debit_total = 0
-                    for voucher_line_id in voucher_line_ids:
-                        debit_total += voucher_line_id.debit
-                    expense_total += debit_total
-                    if debit_total != 0:
-                        res={
-                             'name':u'月末结账',
-                             'account_id':expense_account_id.id,
-                             'debit':0,
-                             'credit':debit_total,
-                             }
-                        voucher_line.append(res)
-                #利润结余
-                year_profit_account = company_obj.search([])[0].profit_account
-                if (revenue_total - expense_total) > 0:
-                    res={
-                         'name':u'利润结余',
-                         'account_id':year_profit_account.id,
-                         'debit':0,
-                         'credit':revenue_total - expense_total,
-                         }
-                    voucher_line.append(res)
-                if (revenue_total - expense_total) < 0:
-                    res={
-                         'name':u'利润结余',
-                         'account_id':year_profit_account.id,
-                         'debit':expense_total - revenue_total,
-                         'credit':0,
-                         }
-                    voucher_line.append(res)
-                #生成凭证
-                valus={
-                       'is_checkout':True,
-                       'date':self.date,
-                       'line_ids':[
-                                (0, 0, line) for line in voucher_line],
-                       }
-                voucher = voucher_obj.create(valus)
-                voucher.voucher_done()
-            if self.period_id.month == '12':
-                year_profit_ids = voucher_line_obj.search([
-                                    ('account_id','=',year_profit_account.id),
-                                    ('voucher_id.period_id','=',self.period_id.id)])
-                year_total=0
-                for year_profit_id in year_profit_ids:
-                    year_total += (year_profit_id.credit - year_profit_id.debit)
-                year_line_ids=[{
-                     'name':u'年度结余',
-                     'account_id':company_obj.search([])[0].no_profit_account.id,
-                     'debit':0,
-                     'credit':year_total,
-                     },{
-                     'name':u'年度结余',
-                     'account_id':year_profit_account.id,
-                     'debit':year_total,
-                     'credit':0,
-                        }]
-                value={'is_checkout':True,
-                       'date':self.date,
-                       'line_ids':[
-                                (0, 0, line) for line in year_line_ids],
-                       }
-                year_account = voucher_obj.create(value)
-                year_account.voucher_done()
-            #关闭会计期间
-            self.period_id.is_closed = True
-            #如果下一个会计期间没有，则创建。
-            next_period = self.env['create.trial.balance.wizard'].compute_next_period_id(self.period_id)
-            if not next_period:
+                    #生成凭证
+                    valus={
+                           'is_checkout':True,
+                           'date':self.date,
+                           'line_ids':[
+                                    (0, 0, line) for line in voucher_line],
+                           }
+                    voucher = voucher_obj.create(valus)
+                    voucher.voucher_done()
                 if self.period_id.month == '12':
-                    self.env['finance.period'].create({'year':self.period_id.year +1,
-                                                       'month':1,})
-                else:
-                    self.env['finance.period'].create({'year':self.period_id.year,
-                                                       'month':self.period_id.month + 1 ,})
-            #显示凭证
-            view = self.env.ref('finance.voucher_form')
-            return {
-                'name': u'月末结账',
-                'view_type': 'form',
-                'view_mode': 'form',
-                'views': [(view.id, 'form')],
-                'res_model': 'voucher',
-                'type': 'ir.actions.act_window',
-                'res_id': voucher.id,
-                'limit': 300,
-            }
+                    year_profit_ids = voucher_line_obj.search([
+                                        ('account_id','=',year_profit_account.id),
+                                        ('voucher_id.period_id','=',self.period_id.id)])
+                    year_total=0
+                    for year_profit_id in year_profit_ids:
+                        year_total += (year_profit_id.credit - year_profit_id.debit)
+                    year_line_ids=[{
+                         'name':u'年度结余',
+                         'account_id':company_obj.search([])[0].remain_account.id,
+                         'debit':0,
+                         'credit':year_total,
+                         },{
+                         'name':u'年度结余',
+                         'account_id':year_profit_account.id,
+                         'debit':year_total,
+                         'credit':0,
+                            }]
+                    value={'is_checkout':True,
+                           'date':self.date,
+                           'line_ids':[
+                                    (0, 0, line) for line in year_line_ids],
+                           }
+                    year_account = voucher_obj.create(value)
+                    year_account.voucher_done()
+                #关闭会计期间
+                self.period_id.is_closed = True
+                #如果下一个会计期间没有，则创建。
+                next_period = self.env['create.trial.balance.wizard'].compute_next_period_id(self.period_id)
+                if not next_period:
+                    if self.period_id.month == '12':
+                        self.env['finance.period'].create({'year':str(int(self.period_id.year) +1),
+                                                           'month':'1',})
+                    else:
+                        self.env['finance.period'].create({'year':self.period_id.year,
+                                                           'month':str(int(self.period_id.month) + 1),})
+                #显示凭证
+                view = self.env.ref('finance.voucher_form')
+                return {
+                    'name': u'月末结账',
+                    'view_type': 'form',
+                    'view_mode': 'form',
+                    'views': [(view.id, 'form')],
+                    'res_model': 'voucher',
+                    'type': 'ir.actions.act_window',
+                    'res_id': voucher.id,
+                    'limit': 300,
+                }
 
     @api.multi
     def button_counter_checkout(self):
