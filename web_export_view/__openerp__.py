b9f6de8850f01b4a2a51f9aa254ec33fa41e8ca1
gilbert-yuan <gilbert@osbzr.com>
2016-04-28 00:26:47 +0800

    web_export_view 改进可以根据model添加 对应的 模板,在打印的时候就按照模板进行打印. (#208)
    * 添加文件
    
    * 科目余额表进行改进,和bug修复
    
    * test数据的完善
    
    * 利润表,和资产负债表的初步完成.
    
    * test数据有填写失误的地方.
    
    * web_export_view 改进可以根据model添加  对应的 模板,在打印的时候就按照模板进行打印.
    
    * copy_right 添加

---
+++ web_export_view/__openerp__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2012 Agile Business Group sagl (<http://www.agilebg.com>)
+#    Copyright (C) 2012 Domsense srl (<http://www.domsense.com>)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Export Current View',
+    'version': '8.0.1.2.0',
+    'category': 'Web',
+    'author': "Agile Business Group,Odoo Community Association (OCA)",
+    'website': 'http://www.agilebg.com',
+    'license': 'AGPL-3',
+    'depends': [
+        'web',
+    ],
+    'data': [
+        'view/web_export_view.xml',
+    ],
+    'qweb': [
+        'static/src/xml/web_export_view_template.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
