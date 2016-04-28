c109a768df41a6a3617e46aa17eba02d9d1bba10
袁文浦 <gilbert@osbzr.com>
2016-04-28 17:31:59 +0800

    注释的添加.

--- finance/balance_sheet.py
+++ finance/balance_sheet.py
@@ -10,6 +10,8 @@ ISODATETIMEFORMAT = "%Y-%m-%d %H:%M:%S"
 
 
 class BalanceSheet(models.Model):
+    """资产负债表"""
+
     _name = "balance.sheet"
     balance = fields.Char(u'资产')
     line_num = fields.Integer(u'行次')
@@ -27,17 +29,20 @@ class BalanceSheet(models.Model):
 
 
 class create_balance_sheet_wizard(models.TransientModel):
+    """创建资产负债 和利润表的 wizard"""
     _name = "create.balance.sheet.wizard"
     period_id = fields.Many2one('finance.period', string='会计期间')
 
     @api.multi
     def compute_balance(self, parameter_str, period_id, compute_field_list):
+        """根据所填写的 科目的code 和计算的字段 进行计算对应的资产值"""
         if parameter_str:
             parameter_str_list = parameter_str.split('~')
             subject_vals = []
             subject_ids = self.env['finance.account'].search([('code', '>=', parameter_str_list[0]), ('code', '<=', parameter_str_list[1])])
             trial_balances = self.env['trial.balance'].search([('subject_name_id', 'in', [subject.id for subject in subject_ids]), ('period_id', '=', period_id.id)])
             for trial_balance in trial_balances:
+                # 根据参数code 对应的科目的 方向 进行不同的操作
                 if trial_balance.subject_name_id.balance_directions == 'in':
                     subject_vals.append(trial_balance[compute_field_list[0]] - trial_balance[compute_field_list[1]])
                 elif trial_balance.subject_name_id.balance_directions == 'out':
@@ -49,6 +54,7 @@ class create_balance_sheet_wizard(models.TransientModel):
 
     @api.multi
     def create_balance_sheet(self):
+        """ 资产负债表的创建 """
         balance_wizard = self.env['create.trial.balance.wizard'].create({'period_id': self.period_id.id})
         balance_wizard.create_trial_balance()
         view_id = self.env.ref('finance.balance_sheet_tree_wizard').id
@@ -61,7 +67,7 @@ class create_balance_sheet_wizard(models.TransientModel):
                                      'ending_balance': self.compute_balance(balance_sheet_obj.ending_balance_formula, self.period_id, current_period_field),
                                      'beginning_balance_two': self.compute_balance(balance_sheet_obj.beginning_balance_two_formula, period, year_begain_field),
                                      'ending_balance_two': self.compute_balance(balance_sheet_obj.ending_balance_two_formula, self.period_id, current_period_field)})
-        return {
+        return {     # 返回生成资产负债表的数据的列表
             'type': 'ir.actions.act_window',
             'name': '资产负债表',
             'view_type': 'form',
@@ -76,6 +82,7 @@ class create_balance_sheet_wizard(models.TransientModel):
 
     @api.multi
     def create_profit_statement(self):
+        """生成利润表"""
         balance_wizard = self.env['create.trial.balance.wizard'].create({'period_id': self.period_id.id})
         balance_wizard.create_trial_balance()
         view_id = self.env.ref('finance.profit_statement_tree').id
@@ -85,9 +92,9 @@ class create_balance_sheet_wizard(models.TransientModel):
         for balance_sheet_obj in balance_sheet_objs:
             balance_sheet_obj.write({'cumulative_occurrence_balance': self.compute_profit(balance_sheet_obj.cumulative_occurrence_balance_formula, self.period_id, year_begain_field),
                                      'current_occurrence_balance': self.compute_profit(balance_sheet_obj.current_occurrence_balance_formula, self.period_id, current_period_field)})
-        return {
+        return {      # 返回生成利润表的数据的列表
             'type': 'ir.actions.act_window',
-            'name': '资产负债表',
+            'name': '利润表',
             'view_type': 'form',
             'view_mode': 'tree',
             'res_model': 'profit.statement',
@@ -100,6 +107,7 @@ class create_balance_sheet_wizard(models.TransientModel):
 
     @api.multi
     def compute_profit(self, parameter_str, period_id, compute_field_list):
+        """ 根据传进来的 的科目的code 进行利润表的计算 """
         if parameter_str:
             parameter_str_list = parameter_str.split('~')
             subject_vals = []
@@ -116,6 +124,7 @@ class create_balance_sheet_wizard(models.TransientModel):
 
 
 class ProfitStatement(models.Model):
+    """资产负债表"""
     _name = "profit.statement"
     balance = fields.Char(u'项目')
     line_num = fields.Integer(u'行次')
