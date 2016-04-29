f95e16b98d64be4a7a63ae084b60e6f8a1d33468
袁文浦 <gilbert@osbzr.com>
2016-04-29 11:42:10 +0800

    homepage中的bug fix :布局的调整,和图片不能正常显示的问题.以及添加字段view_id用来解决多视图对应一个model的问题

--- home_page/static/src/js/home_page.js
+++ home_page/static/src/js/home_page.js
@@ -7,12 +7,17 @@ openerp.home_page = function(instance, local) {
         start: function() {
             var num=this.get('value');
             var self=this
-            self.$el.append("<div><div class='outter_div'></div>\
-                <div class='top_div' ></div><div class='left_div'></div></div>")
+            self.$el.append("<div style='top:150PX'>\
+                                    <div style='float:left;width:80%;'>\
+                                        <div style='float:left;width:20%;' class='top_div'></div>\
+                                        <div style='float:right;width:80%;' class='main_div'></div>\
+                                    </div>\
+                                    <div style='float:right;width:20%;' class='right_div'></div>\
+                            </div>")
             new instance.web.Model("home.page").call("get_action_url").then(function(result){
                 var index=0
                 var center_main_table="<div class='center_class'><table border='1' cellpadding='3' class='oe_table_main'></table></div>"
-                self.$el.find('.outter_div').append(center_main_table);
+                self.$el.find('.main_div').append(center_main_table);
                 self.result_main=result['main']
                 for(var i=0;i<2;i++){
                     var center_tr_html="<tr class='oe_tr_"+i+"'></tr>"
@@ -22,16 +27,16 @@ openerp.home_page = function(instance, local) {
                         if (result_one===undefined){
                             result_one=[0,0,0,0,'web/binary/company_logo?db=hahh&company=1']
                         }else{
-                            result_one[4]="/web/binary/image?model=home.page&id="+result_one[4]+"&field=image" 
+                            result_one[4]="/web/binary/image?model=home.page&id="+result_one[4]+"&field=image"
                         }
-                        var center_html_str="<td ><img class='oe_img oe_main_link_"+index+"' id='"+index+"' height='200' \
-                         width='200' src='"+result_one[4]+"' alt='暂无快捷菜单链接' />\
+                        console.log(result_one)
+                        var center_html_str="<td ><img class='oe_img oe_main_link_"+index+"' id='"+index+"' height='150' \
+                         width='150' src='"+result_one[4]+"' alt='暂无快捷菜单链接' />\
                         <br/><p>"+result_one[0]+"</p>\
                         </td>"
                         self.$el.find('.oe_tr_'+i+'').append(center_html_str);
                         if (result_one[0]!=result_one[1]){
                             self.$(".oe_main_link_"+index+"").click(function() {
-                                console.log(self.result_main[this.id]);
                                  self.do_action({
                                     type: 'ir.actions.act_window',
                                     res_model: (self.result_main[this.id])[2],
@@ -51,14 +56,14 @@ openerp.home_page = function(instance, local) {
                 self.result_top=result['top']
                 for(var i=0;i< 5;i++){
                     if (self.result_top[i]!==undefined){
-                        var left_html_str = $("<a><p class='oe_p oe_top_link_"+i+"' id='"+i+"'>"+self.result_top[i][0]+"</p></a>");
+                        var left_html_str = $("<p ><a class='oe_p oe_top_link_"+i+"' id='"+i+"'>"+self.result_top[i][0]+"</a></p> ");
                         self.$el.find('.top_div').append(left_html_str);
                         self.$(".oe_top_link_"+i+"").click(function() {
                              self.do_action({
                                 type: 'ir.actions.act_window',
                                 res_model: (self.result_top[this.id])[2],
                                 view_mode: 'list',
-                                view_type: self.result_top[this.id][1],
+                                /*view_type: self.result_top[this.id][1],*/
                                 views: [[self.result_top[this.id][5], 'list'],[false, 'form']],
                                 domain:self.result_top[this.id][3],
                                 context:self.result_top[this.id][4],
@@ -68,18 +73,18 @@ openerp.home_page = function(instance, local) {
                         });
                     }
                 }
-                self.result_quick=result['left']
+                self.result_quick=result['right']
                 for(var i=0;i< 8;i++){
                     if (self.result_quick[i]!==undefined){
                         var left_html_str = $("<a><p class='oe_p oe_quick_link_"+i+"' id='"+i+"'>"+self.result_quick[i][0]+"</p></a>");
-                        self.$el.find('.top_div').append(left_html_str);
+                        self.$el.find('.right_div').append(left_html_str);
                         self.$(".oe_quick_link_"+i+"").click(function() {
                              self.do_action({
                                 type: 'ir.actions.act_window',
                                 res_model: (self.result_quick[this.id])[2],
                                 view_mode: 'list',
                                 name:self.result_quick[this.id][6],
-                                view_type: self.result_quick[this.id][1],
+                                /*view_type: self.result_quick[this.id][1],*/
                                 views: [[self.result_quick[this.id][5], 'list'],[false, 'form']],
                                 domain:self.result_quick[this.id][3],
                                 context:self.result_quick[this.id][4],
