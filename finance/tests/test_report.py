8b0bea044da79b9d4b3a8ba5525804ba5d9ec44d
开阖软件 Jeff Wang <jeff@osbzr.com>
2016-05-05 10:19:05 +0800

    merge others code (#243)
    * [FIX]finance程序错误
    
    * [FIX]finance 向导应该是TransientModel
    
    * [UPD]finance 整理科目数据和测试用例
    
    * [UPD]error in xml
    
    * [UPD]添加测试用例
    
    * [UPD]修改描述
    
    * 去掉无用的代码
    
    * [ADD]添加报表测试用例
    
    * retry
    
    * typo
    
    * [UPD]先测试异常流程
    
    * 解决依赖库问题
    
    * [UPD]避免ci失败
    
    * [FIX]缺一个标签
    
    * [UPD]修复ci
    
    * [ADD]权限

--- finance/tests/test_report.py
+++ finance/tests/test_report.py
@@ -112,7 +112,6 @@ class test_report(TransactionCase):
             balance_sheet_obj.cumulative_occurrence_balance_formula = ''
         report.create_profit_statement()
 
-
 class test_checkout_wizard(TransactionCase):
     
     def setUp(self):
@@ -168,6 +167,7 @@ class test_checkout_wizard(TransactionCase):
         wizard.button_counter_checkout()
         with self.assertRaises(except_orm):
             wizard.button_counter_checkout()
+
         #公司科目未配置报错
         company_pro = self.env.ref('base.main_company')
         company_pro.profit_account = False
@@ -177,3 +177,4 @@ class test_checkout_wizard(TransactionCase):
         company_pro.remain_account = False
         with self.assertRaises(except_orm):
             wizard.button_checkout()
+
