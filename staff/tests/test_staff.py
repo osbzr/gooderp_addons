91d3e068aad9c2214edd98abf50ee169e6ca0881
JasonChen <jason@osbzr.com>
2016-05-05 13:59:58 +0800

    [fix]staff err

--- staff/tests/test_staff.py
+++ staff/tests/test_staff.py
@@ -6,11 +6,13 @@ from openerp.exceptions import except_orm, ValidationError
 class test_staff(TransactionCase):
     
     def test_get_image(self):
-        '''拿到用户头像'''
+        '''拿到用户头像,职位的onchange'''
         staff_pro = self.env['staff'].create({
                                               'name':'DemoUser',
-                                              'user_id':1})
+                                              'user_id':1,
+                                              'job_id':self.env.ref('staff.staff_job_1').id})
         staff_pro._get_image()
+        staff_pro.onchange_job_id()
 
         
    
