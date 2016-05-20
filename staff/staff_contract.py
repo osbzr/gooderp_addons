caa31a2c6f7e4785d54783d71e9638b0afc902c5
jeffery chen fan <jeffery9@gmail.com>
2016-05-20 12:33:00 +0800

    pep8 for module staff

--- staff/staff_contract.py
+++ staff/staff_contract.py
@@ -3,10 +3,10 @@ from openerp import fields, models, api
 
 
 class staff_contract(models.Model):
-    _name='staff.contract'
-    
-    staff_id = fields.Many2one('staff',u'员工',required=True)
-    
-    over_date = fields.Date(u'到期日',required=True)
+    _name = 'staff.contract'
+
+    staff_id = fields.Many2one('staff', u'员工', required=True)
+
+    over_date = fields.Date(u'到期日', required=True)
     basic_wage = fields.Float(u'基础工资')
-    job_id = fields.Many2one('staff.job',u'岗位',required=True)
+    job_id = fields.Many2one('staff.job', u'岗位', required=True)
