88e8de3977b6d0cecffd3485a4f4054bdab8fb82
孙昊 <sunhao@192.168.2.105>
2016-07-19 14:41:15 +0800

    检测各种定价策略不唯一的情况

--- core/core.py
+++ core/core.py
@@ -378,7 +378,7 @@ class pricing(models.Model):
             return warehouse_pricing
         if len(warehouse_pricing) > 1:
             raise except_orm(u'错误',
-                             u'适用于 %s,%s,%s 的价格策略不唯一'%(warehouse.name,
+                             u'适用于 %s,%s 的价格策略不唯一'%(warehouse.name,
                                                              date))
         # 客户类别,商品满足条件
         ccg_pricing = self.search([
@@ -393,7 +393,6 @@ class pricing(models.Model):
         if len(ccg_pricing) == 1:
             return ccg_pricing
         if len(ccg_pricing) > 1:
-            print 'qwdfb'
             raise except_orm(u'错误',
                              u'适用于 %s,%s,%s 的价格策略不唯一'%(partner.c_category_id.name,
                                                              goods.name,
