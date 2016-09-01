b4e7383d3e218afb2f5a15cef8792ff0c8087bec
xuanyimin <freemanxuan@163.com>
2016-09-01 19:23:33 +0800

    固定资产

---
+++ asset/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "GOODERP 资产模块",
+    'author': "德清武康开源软件工作室",
+    'website': "无",
+    'category': 'gooderp',
+    "description": """
+        实现平均年限法的固定资产初始化，采购，变更，处理。
+    """,
+    'version': '8.0.0.1',
+    'depends': ['base', 'finance', 'money'],
+    'data': [
+        'view/asset.xml',
+        'data/asset_data.xml',
+    ],
+    'demo': [
+    ],
+    'qweb': [
+        "static/src/xml/*.xml",
+    ],
+}
