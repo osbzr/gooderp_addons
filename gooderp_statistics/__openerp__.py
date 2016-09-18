eb1054dff2f9199569492fb8b2dd0b02c800a520
jeff <jeff@osbzr.com>
2016-09-18 18:01:21 +0800

    [ADD]statistics

---
+++ gooderp_statistics/__openerp__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "GOODERP Statistics",
+    "version": "0.1",
+    "author": 'ZhengXiang',
+    "website": "http://www.osbzr.com",
+    "category": "Generic Modules",
+    "description": """
+    """,
+    "data": [
+        'views/web_need.xml',
+    ],
+    'installable': True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
