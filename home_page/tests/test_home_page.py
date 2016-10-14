c27a8c3bfdcf9436e1c78ebf18924306c5be7d4e
静静 <giblert@osbzr.com>
2016-10-14 10:06:33 +0800

    home_page 测试数据 bug修复

--- home_page/tests/test_home_page.py
+++ home_page/tests/test_home_page.py
@@ -9,11 +9,12 @@ class test_home_page(TransactionCase):
     """
     def test_home_page(self):
         """测试首页的显示情况"""
-        partner_action = self.env.ref('base.action_res_users')
+        partner_action = self.env.ref('base.action_partner_form')
+        partner_credit = self.env.ref('base.field_res_partner_credit_limit')
         self.env['home.page'].create({'sequence': 10, 'action': partner_action.id, 'menu_type': 'all_business',
                                       'domain': '[]', 'context': '{}'})
         self.env['home.page'].create({'sequence': 10, 'action': partner_action.id, 'menu_type': 'amount_summary',
-                                      'domain': [], 'context': {},'note_one':'partner','compute_field_one':'companies_count'})
+                                      'domain': [], 'context': {},'note_one':'partner','compute_field_one':partner_credit.id})
         self.env['home.page'].create({'sequence': 10, 'action': partner_action.id, 'menu_type': 'report',
                                       'domain': [], 'context': {}})
         self.env['home.page'].get_action_url()
@@ -21,8 +22,8 @@ class test_home_page(TransactionCase):
     def test_onchange_action(self):
         '''测试 onchange_action
         '''
-        partner_action = self.env.ref('base.action_res_users')
-        partner_action = self.env['home.page'].create({'sequence': 10, 'action': partner_action.id, 'menu_type': 'all_business',
+        partner_action_view = self.env.ref('base.action_partner_form')
+        partner_action = self.env['home.page'].create({'sequence': 10, 'action': partner_action_view.id, 'menu_type': 'all_business',
                                       'domain': '[]', 'context': '{}'})
         result = partner_action.onchange_action()
         real_result = {'domain': {'view_id': [('model', '=', u'res.partner'), ('type', '=', 'tree')]}}
