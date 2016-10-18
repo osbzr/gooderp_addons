c886cba7249c2c9ec811d1aa4e69b80643e72ebc
静静 <giblert@osbzr.com>
2016-10-18 09:18:06 +0800

    finance test

--- sell/tests/test_finance.py
+++ sell/tests/test_finance.py
@@ -13,4 +13,10 @@ class test_month_product_cost(TransactionCase):
         checkout_wizard_row = self.env['checkout.wizard'].create({'date':'2016-01-31','period_id':self.period_id.id})
         with self.assertRaises(UserError):
             checkout_wizard_row.button_checkout()
+        sell_order_rows = self.env['sell.order'].search({})
+        [sell_order_row.sell_order_done() for sell_order_row in sell_order_rows]
+        sell_delivery_rows = self.env['sell.delivery'].search({})
+        [sell_delivery_row.sell_delivery_done() for sell_delivery_row in sell_delivery_rows]
+        sell_adjust_rows = self.env['sell.adjust'].search({})
+        [sell_adjust_row.sell_adjust_done() for sell_adjust_row in sell_adjust_rows]
         self.env['month.product.cost'].generate_issue_cost(self.period_id)
