6b78789b186da1f3c5e33989020dbd1f63fa370d
静静 <giblert@osbzr.com>
2016-10-25 16:37:15 +0800

    test web_export_view_good sell的test

--- sell/tests/test_ReportDocx.py
+++ sell/tests/test_ReportDocx.py
@@ -3,6 +3,7 @@ from odoo.tests.common import TransactionCase
 from odoo.exceptions import UserError
 from odoo.addons.report_docx.report.report_docx import DataModelProxy
 from odoo.tools import misc
+import random,os
 
 class test_ReportDocx(TransactionCase):
 
@@ -12,6 +13,11 @@ class test_ReportDocx(TransactionCase):
         self.ir_actions = self.env.ref('sell.report_sell_order_1')
         self.sell_order = self.env.ref('sell.sell_order_1')
         self.report_docx_sell = self.ir_actions._lookup_report('sell.order')
+
+        self.ir_actions = self.env.ref('sell.report_sell_order_2')
+        self.sell_order = self.env.ref('sell.sell_order_1')
+        self.report_pdf_sell = self.ir_actions._lookup_report('sell.order')
+
 
     def test_lookup_report(self):
         '''测试docx报表模板'''
@@ -29,9 +35,9 @@ class test_ReportDocx(TransactionCase):
         doxc_file = self.report_docx_sell.create(self.cr, self.uid, self.sell_order.id, self.ir_actions, self.env.context)
         self.report_docx_sell._save_file(misc.file_open('sell/tests/sell.order.docx').name,doxc_file)
 
-
-    def test_render_to_pdf(self):
-        self.report_docx_sell.render_to_pdf(misc.file_open('sell/tests/sell.order.docx').name)
+    # def test_render_to_pdf(self):
+    #     self.report_pdf_sell.render_to_pdf( os.path.join(os.getcwd(), 'temp_%s_%s.%s' %
+    #                         (os.getpid(), random.randint(1, 10000), 'docx')))
 
     def test_datamodelproxy(self):
         data=DataModelProxy([{"type":'selection'}])
