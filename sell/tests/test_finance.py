8b6d6da99a5c3653b5bfde3e06214f5c27879f37
静静 <giblert@osbzr.com>
2016-11-01 16:05:24 +0800

    测试数据修改导致覆盖率降低。修复。

--- sell/tests/test_finance.py
+++ sell/tests/test_finance.py
@@ -15,13 +15,13 @@ class test_month_product_cost(TransactionCase):
             checkout_wizard_row.button_checkout()
         # sell_delivery_rows = self.env['sell.delivery'].search([('type','=','others')])
         wh_in_rows = self.env['wh.in'].search([])
-        wh_in_rows.write({'date':'2016-01-18 18:00:00'})
+        wh_in_rows.write({'date':'2016-01-31 18:00:00'})
         wh_out_rows = self.env['wh.out'].search([('name', '=', self.env.ref('warehouse.wh_out_whout1').name)])
-        wh_out_rows.write({'date':'2016-01-18 18:00:00'})
+        wh_out_rows.write({'date':'2016-01-31 18:00:00'})
         # wh_internal_rows = self.env['wh.internal'].search([])
         # [sell_delivery_row.sell_delivery_done() for sell_delivery_row in sell_delivery_rows]
         [wh_in_row.approve_order() for wh_in_row in wh_in_rows]
         [wh_out_row.approve_order() for wh_out_row in wh_out_rows]
         # [wh_internal_row.approve_order() for wh_internal_row in wh_internal_rows]
-        self.env['month.product.cost'].generate_issue_cost(self.env['finance.period'].get_date_now_period_id())
+        self.env['month.product.cost'].generate_issue_cost(self.period_id.id)
 
