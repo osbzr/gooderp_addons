4ff0fe1d988e49291f66e548ec5e10e2e1e1c616
Judy <judy@osbzr.com>
2016-11-22 16:38:58 +0800

    [ADD] 添加core、staff模块 check_user_id 测试数据

--- staff/tests/test_staff.py
+++ staff/tests/test_staff.py
@@ -57,3 +57,22 @@ class test_mail_message(TransactionCase):
         job.nextcall = (datetime.now() + timedelta(hours=8)).strftime('%Y-%m-%d %H:%M:%S')
         job.doall = True
         self.env['mail.message'].staff_birthday_message()
+
+class test_res_users(TransactionCase):
+
+    def test_check_user_id(self):
+        ''' 测试 一个用户只能对应一个员工 '''
+        # core 模块里
+        user = self.env.ref('base.user_demo')
+        self.env.ref('staff.lili').user_id = user.id
+
+        with self.assertRaises(ValidationError):
+            self.env.ref('staff.staff_1').user_id = user.id
+
+        # staff 模块里
+        user = self.env.ref('base.user_demo')
+        admin_user = self.env.ref('base.user_root')
+        user.staff_id = self.env.ref('staff.lili').id
+
+        with self.assertRaises(ValidationError):
+            admin_user.staff_id = self.env.ref('staff.lili').id
