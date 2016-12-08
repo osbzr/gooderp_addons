7007d2e894c55c413721774555216dae277b739e
静静 <giblert@osbzr.com>
2016-12-08 10:54:45 +0800

    #879 请假单

--- staff/leave.py
+++ staff/leave.py
@@ -17,7 +17,7 @@ class staff_leave(models.Model):
     leave_type = fields.Selection([('no_pay', u'无薪'),('with_pay', u'带薪'),
                                    ('compensation_day', u'补偿日数'),('sick_leave', u'病假')],
                                     default='no_pay', string=u'准假类型')
-    leave_date = fields.Integer(u'请假时长', compute='onchange_data_start_or_stop', default=1)
+    leave_dates = fields.Integer(u'请假时长', compute='onchange_data_start_or_stop', default=1)
 
     @api.one
     @api.depends('date_start','date_stop')
@@ -25,6 +25,7 @@ class staff_leave(models.Model):
         if self.date_start and self.date_stop:
             date_start_struct = time.strptime(self.date_start, "%Y-%m-%d %H:%M:%S")  # 字符串转换成time类型
             date_stop_struct = time.strptime(self.date_stop, "%Y-%m-%d %H:%M:%S")  # 字符串转换成time类型
+            print date_start_struct[0], date_start_struct[1],  date_start_struct[2]
             date_start = datetime.datetime(date_start_struct[0], date_start_struct[1], date_start_struct[2])
             date_stop = datetime.datetime(date_stop_struct[0], date_stop_struct[1], date_stop_struct[2])
-            self.leave_date = (date_stop - date_start).days or 1
+            self.leave_dates = (date_stop - date_start).days or 1
