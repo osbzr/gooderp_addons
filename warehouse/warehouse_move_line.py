4ddaaaa080f864f2c594177311d82cbef32613cd
Judy <judy@osbzr.com>
2016-12-28 11:59:09 +0800

    [ADD] Bug，入库单，通过明细行“添加项目”操作，输入商品，没有带出单价填充到“入库单价”列（扫描输入可以） #958

--- warehouse/warehouse_move_line.py
+++ warehouse/warehouse_move_line.py
@@ -302,6 +302,12 @@ class wh_move_line(models.Model):
             self.cost_unit = cost_unit
             self.cost = cost
 
+        if self.env.context.get('type') == 'in' and self.goods_id and self.goods_qty:
+            cost, cost_unit = self.goods_id.get_suggested_cost_by_warehouse(
+                self.warehouse_id, self.goods_qty, self.lot_id, self.attribute_id)
+
+            self.cost_unit = cost_unit
+
     @api.multi
     @api.onchange('goods_id', 'tax_rate')
     def onchange_goods_id(self):
