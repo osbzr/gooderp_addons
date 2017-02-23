977bd2d921ec9f75e70e1256ed861259dfa806fe
静静 <giblert@osbzr.com>
2017-02-23 11:17:20 +0800

    目录结构调整。

---
+++ finance/models/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import finance
+import trial_balance
+import wizard
+import balance_sheet
+import report
+import res_config
+import voucher_template
+import issue_cost_wizard
+import report_auxiliary_accounting
+import exchange
+
+
---
+++ warehouse/models/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import goods
+import warehouse_move
+import warehouse_order
+import inventory
+import production
+import warehouse_move_line
+import move_matching
+import wizard
+import report
+import res_company
