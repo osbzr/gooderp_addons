7aca07bee1a60609b12413453ece4abc5025bd9b
yxs1205 <645470027@qq.com>
2017-03-20 18:22:06 +0800

    [ADD]增加工资模块的测试

---
+++ staff_wages/tests/test_staff_wages.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from odoo.tests.common import TransactionCase
+
+class test_staff_wages(TransactionCase):
+
+    def setUp(self):
+        super(test_staff_wages, self).setUp()
+        self.staff_wages = self.env.ref('staff_wages.staff_wages_lili')
+
+    def test_compute_period_id(self):
+        self.staff_wages._compute_period_id()
+        self.assertTrue(self.staff_wages.name == self.env.ref('finance.period_201703'))
+    
+    def test_total_amount_wage(self):
+        for line in self.staff_wages.line_ids:
+            line.change_social_security()
+        self.staff_wages.total_amount_wage()
+        self.assertAlmostEqual(self.staff_wages.totoal_amount, 2463.64)
+        self.assertAlmostEqual(self.staff_wages.totoal_wage, 2863.64)
+        self.assertAlmostEqual(self.staff_wages.totoal_endowment, 100)
+        self.assertAlmostEqual(self.staff_wages.totoal_health, 100)
+        self.assertAlmostEqual(self.staff_wages.totoal_unemployment, 100)
+        self.assertAlmostEqual(self.staff_wages.totoal_housing_fund, 100)
+        self.assertAlmostEqual(self.staff_wages.totoal_personal_tax, 0)
+
+
