1ae6d36e218e5113f8ae7ba1c550da05e1699e35
开阖软件 Jeff Wang <jeff@osbzr.com>
2017-04-10 16:24:36 +0800

    close #1223

--- report_docx/report/report_docx.py
+++ report_docx/report/report_docx.py
@@ -151,7 +151,7 @@ class ReportDocx(report_sxw):
         html = """<html style="height: 100%">
             <head>
                 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
-                <meta http-equiv="content-type" content="text/html; charset=utf-8"/>'
+                <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
             </head>
             <body>
             """
