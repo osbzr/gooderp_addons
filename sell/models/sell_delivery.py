e6f116777fde47c98aea3f4f8e93270c544ea878
flora <flora@osbzr.com>
2017-05-03 10:00:24 +0800

    [FIX]修复发货单审核时生成出库凭证借贷方不平的bug

--- sell/models/sell_delivery.py
+++ sell/models/sell_delivery.py
@@ -354,7 +354,7 @@ class sell_delivery(models.Model):
                 # 缺货审核发货单时不产生出库凭证
                 return
             sum_amount += cost
-            if line.amount:  # 贷方明细
+            if line.cost:  # 贷方明细
                 self._create_voucher_line(line.goods_id.category_id.account_id,
                                           0, cost, voucher, line.goods_id, line.goods_qty)
         if sum_amount:  # 借方明细
