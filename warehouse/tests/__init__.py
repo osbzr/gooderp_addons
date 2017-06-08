11f74760f0b90dc1d91a8d20ead0121627291b81
flora <flora@osbzr.com>
2017-06-08 12:12:38 +0800

    [FIX]加上TEST引入

--- warehouse/tests/__init__.py
+++ warehouse/tests/__init__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-# import test_goods
-# import test_inventory
-# import test_move_line
-# import test_production
+import test_goods
+import test_inventory
+import test_move_line
+import test_production
 import test_report
-# import test_non_active_report
-# import test_warehouse
-# import test_warehouse_order
-# import test_qc_rule
+import test_non_active_report
+import test_warehouse
+import test_warehouse_order
+import test_qc_rule
 
