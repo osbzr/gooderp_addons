d201d9c226349f61b281ef8f80677c4e7fcd749e
DESKTOP-RJLU035\PengYB <pengyb@topodoo.com>
2017-06-27 18:11:55 +0800

    优化背景颜色 ，增加对于select类型字段的支持； 增加在列表上多做选以后的批量删除。

--- top_tree_widget/static/src/js/web_tree_bgcolor.js
+++ top_tree_widget/static/src/js/web_tree_bgcolor.js
@@ -14,13 +14,28 @@ odoo.define('web.WebTreeBackColor', function (require) {
             // 显示内容格式化
             self.widget=null;
             var value = this._super.apply(self, arguments);
+            var bgcolor = self.bgcolor;
+
+            var bg_condition = self.bg_condition;
+
+            if (self.bg_select !== null && self.bg_select !== undefined && self.bg_select !== ''){
+                var bg_select = JSON.parse(self.bg_select);
+                for(var key in bg_select){  
+                    console.log("key:%s",key);
+                }
+                this.bgcolor = bg_select[value]
+            }
 
             // 增加背景色
+            if (this.bgcolor == null || this.bgcolor == undefined || this.bgcolor == ''){
+                return value;
+            }
+
             var ret = QWeb.render('ListView.row.bgcolor', {widget: this, value: value});
 
             //修改显示大小
-            if (self.bgsize !== null || self.bgsize !== undefined || self.bgsize !== ''){
-                return '<h'+self.bgsize+'>'+ret+'</h'+self.bgsize+'>';
+            if (this.bgsize !== null && this.bgsize !== undefined && self.bgsize !== ''){
+                return '<h'+this.bgsize+'>'+ret+'</h'+this.bgsize+'>';
             }
         }
     });
