788f1df20d1f3be6e09919cca2ad4596d67cbc95
flora <flora@osbzr.com>
2017-08-11 17:59:41 +0800

    [ADD]pos模块增加pos设置和pos产品分类菜单，会话中付款后生成发货单

--- gooderp_pos/models/res_users.py
+++ gooderp_pos/models/res_users.py
@@ -7,9 +7,9 @@ from odoo.exceptions import UserError
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
-    pos_security_pin = fields.Char(string='Security PIN', size=32, help='A Security PIN used to protect sensible functionality in the Point of Sale')
+    pos_security_pin = fields.Char(string=u'安全PIN', size=32, help=u'在POS中一个安全PIN是用来保护敏感功能的')
 
     @api.constrains('pos_security_pin')
     def _check_pin(self):
         if self.pos_security_pin and not self.pos_security_pin.isdigit():
-            raise UserError(_("Security PIN can only contain digits"))
+            raise UserError(u"安全PIN只能包含数字")
