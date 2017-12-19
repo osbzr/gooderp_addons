fad9facdaeb208ed9734b515677f2e09e3ea78fc
Judystudy <judy@osbzr.com>
2017-12-19 09:49:20 +0800

    [FIX] ci fail; 销售/采购订单生成销售出库单、采购入库单单价为0

--- auto_exchange/tests/test_auto_exchange.py
+++ auto_exchange/tests/test_auto_exchange.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo.tests.common import TransactionCase
+from odoo.exceptions import UserError
 
 
 class TestAutoExchange(TransactionCase):
@@ -7,8 +8,13 @@ class TestAutoExchange(TransactionCase):
     def setUp(self):
         '''准备基本数据'''
         super(TestAutoExchange, self).setUp()
-        self.bsd = self.env.ref('base.BSD')
+        self.usd = self.env.ref('base.USD')
 
     def test_get_exchange(self):
         ''' 测试 自动取汇率 '''
-        self.bsd.get_exchange()
+        self.usd.get_exchange()
+
+        # 中国银行找不到您的(%s)币别汇率
+        # bsd = self.env.ref('base.BSD')
+        # with self.assertRaises(UserError):
+        #     bsd.get_exchange()
