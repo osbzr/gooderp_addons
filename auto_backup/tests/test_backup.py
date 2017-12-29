dc16d6d792d01b0c7ed9b172688d3da2ee1ae0b8
Judystudy <judy@osbzr.com>
2017-12-29 14:30:01 +0800

    [FIX] 修复 ci fail; 添加 goods_class 测试数据

--- auto_backup/tests/test_backup.py
+++ auto_backup/tests/test_backup.py
@@ -8,7 +8,7 @@ class TestDbBackup(TransactionCase):
         ''' 准备数据 '''
         super(TestDbBackup, self).setUp()
         self.obj = self.env.get('db.backup')
-        self.back = self.env.ref('auto_backup.backup_demo')
+        # self.back = self.env.ref('auto_backup.backup_demo')
 
     def test_schedule_backup_pgtool(self):
         ''' 测试：数据库自动备份 '''
