36483b60d697f5ba5efe8d6c130e0df3bb884ab8
Judystudy <judy@osbzr.com>
2018-01-15 16:57:40 +0800

    [ADD] #IHCXO 快递面单增加捡货单号和格子号

--- warehouse_wave/models/express_menu.py
+++ warehouse_wave/models/express_menu.py
@@ -58,13 +58,22 @@ class WhMove(models.Model):
         return shipping_type_config
 
     def get_sender(self, ware_hosue_row, pakge_sequence):
+        add_info = u'；格子号:'
+        if pakge_sequence:
+            add_info += pakge_sequence
+        else:
+            add_info += u' '
+        add_info += u'；拣货单:'
+        if self.wave_id:
+            add_info += self.wave_id.name
+
         sender = dict(Company=ware_hosue_row.company_id.name,
                       Name=ware_hosue_row.principal_id.name or ware_hosue_row.company_id.name or '',
                       Mobile=ware_hosue_row.principal_id.work_phone or ware_hosue_row.company_id.phone,
                       ProvinceName=ware_hosue_row.province_id.name or u'上海',
                       CityName=ware_hosue_row.city_id.city_name or u'上海',
                       ExpAreaName=ware_hosue_row.county_id.county_name or u'浦东新区',
-                      Address=(ware_hosue_row.detail_address or u'金海路2588号B-213'))
+                      Address=((ware_hosue_row.detail_address or u'金海路2588号B-213') + add_info))
         return sender
 
     def get_receiver_goods_message(self):
