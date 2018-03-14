b778d732cfd1007067ec4bd93e2b446825a9b896
Judystudy <judy@osbzr.com>
2018-03-14 16:48:46 +0800

    [FIX] 自动备份测试数据

--- auto_backup/tests/test_backup.py
+++ auto_backup/tests/test_backup.py
@@ -6,13 +6,14 @@ import socket
 class TestDbBackup(TransactionCase):
 
     def setUp(self):
-        ''' 准备数据 '''
+        ''' setUp Data '''
         super(TestDbBackup, self).setUp()
-        self.ip = socket.gethostbyname("www.gooderp.org")
-        confs = self.env['db.backup'].get_db_list(host="%s" % self.ip, port='8888')
-        name = confs and confs[0] or 'gooderp'
-        self.backup = self.env['db.backup'].create({
-            "name": "%s" % name,
-            "host": "%s" % self.ip,
-            "port": "8888",
-        })
+        self.backup = self.env.ref('auto_backup.backup_demo')
+
+    def test_schedule_backup(self):
+        ''' Test：Database atuo backup '''
+        self.backup.schedule_backup()
+
+    def test_schedule_backup_pgtool(self):
+        ''' Test：Database atuo backup '''
+        self.backup.schedule_backup_pgtool()
