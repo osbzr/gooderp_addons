f885879da3c0383970a5cb30b6f9630b580dd815
开阖软件 Jeff WANG <jeff@osbzr.com>
2018-04-08 10:16:22 +0800

    [IMP]修正错别字

--- warehouse_wave/__manifest__.py
+++ warehouse_wave/__manifest__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 {
-    'name': '捡货单和打包',
+    'name': '拣货单和打包',
     'version': '11.11',
     'author': "上海开阖软件有限公司",
     'summary': ' ',
     'category': 'gooderp',
     'description':
     '''
-        捡货单的生成
+        拣货单的生成
         打印,删除,及货物的打包.
     ''',
     'data': [
