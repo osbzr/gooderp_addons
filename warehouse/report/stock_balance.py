408e6d3e00c6863e16fb388335f748592974e21f
开阖软件 Jeff WANG <jeff@osbzr.com>
2018-05-08 12:10:30 +0800

    [IMP]科目余额表不显示为0的记录

--- warehouse/report/stock_balance.py
+++ warehouse/report/stock_balance.py
@@ -54,6 +54,7 @@ class ReportStockBalance(models.Model):
 
                 WHERE  wh.type = 'stock'
                   AND line.state = 'done'
+                  AND line.qty_remaining != 0
                   AND ( goods.no_stock is null or goods.no_stock = FALSE)
 
                 GROUP BY wh.name, line.lot, attribute.name, goods.name, goods.id, goods.brand, loc.name, uom.name, uos.name
