847f27f6fc4a99a4d00fcfdbe9042ae45bf29be9
zxy <judy@osbzr.com>
2018-06-07 11:09:31 +0800

    [FIX] 利润表修改

--- finance/models/balance_sheet.py
+++ finance/models/balance_sheet.py
@@ -242,44 +242,11 @@ class CreateBalanceSheetWizard(models.TransientModel):
                         sign_out = True
             trial_balances = self.env['trial.balance'].search([('subject_name_id', 'in', [
                 subject.id for subject in subject_ids]), ('period_id', '=', period_id.id)])
-            no, end_date = self.env['finance.period'].get_period_month_date_range(period_id)
-            begint_date, no = self.env['finance.period'].get_period_month_date_range(
-                self.env['finance.period'].get_year_fist_period_id())
             for trial_balance in trial_balances:
                 if trial_balance.subject_name_id.balance_directions == 'in':
-                    update = trial_balance[compute_field_list[0]]-trial_balance[compute_field_list[1]]
-                    if 'current' in compute_field_list[0]:
-                        checkout_id = self.env['voucher'].search([('is_checkout', '=', True),('period_id', '=', period_id.id)], limit=1)
-                        voucher_line = self.env['voucher.line'].search([('voucher_id', '=', checkout_id.id),('account_id','=',trial_balance.subject_name_id.id)])
-                        if voucher_line and voucher_line.debit != update:
-                            update = voucher_line.credit
-                    else:
-                        checkout_ids = self.env['voucher'].search(
-                            [('is_checkout', '=', True), ('date', '>=', begint_date), ('date', '<=', end_date)])
-                        voucher_line_ids = self.env['voucher.line'].search([('voucher_id', 'in', checkout_ids.ids), (
-                        'account_id', '=', trial_balance.subject_name_id.id)])
-                        voucher_credit = 0
-                        for voucher_line in voucher_line_ids:
-                            voucher_credit += voucher_line.credit
-                        if voucher_credit != update:
-                            update += voucher_credit
-                    subject_vals_in.append(update)
+                    subject_vals_in.append(trial_balance[compute_field_list[0]])
                 elif trial_balance.subject_name_id.balance_directions == 'out':
-                    update = trial_balance[compute_field_list[1]]-trial_balance[compute_field_list[0]]
-                    if 'current' in compute_field_list[1]:
-                        checkout_id = self.env['voucher'].search([('is_checkout', '=', True), ('period_id', '=', period_id.id)], limit=1)
-                        voucher_line = self.env['voucher.line'].search([('voucher_id', '=', checkout_id.id), ('account_id', '=', trial_balance.subject_name_id.id)])
-                        if voucher_line and voucher_line.debit != update:
-                            update = voucher_line.debit
-                    else:
-                        checkout_ids = self.env['voucher'].search([('is_checkout', '=', True), ('date', '>=', begint_date), ('date', '<=', end_date)])
-                        voucher_line_ids = self.env['voucher.line'].search([('voucher_id', 'in', checkout_ids.ids), ('account_id', '=', trial_balance.subject_name_id.id)])
-                        voucher_debit = 0
-                        for voucher_line in voucher_line_ids:
-                            voucher_debit += voucher_line.debit
-                        if voucher_debit != update:
-                            update += voucher_debit
-                    subject_vals_out.append(update)
+                    subject_vals_out.append(trial_balance[compute_field_list[1]])
                 if sign_out and sign_in:  # 方向有借且有贷
                     total_sum = sum(subject_vals_out) - sum(subject_vals_in)
                 else:
