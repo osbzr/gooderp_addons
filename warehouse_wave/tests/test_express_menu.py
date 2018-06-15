a548b047c3751d6c80532e047930c5e1af7855f8
zxy <judy@osbzr.com>
2018-06-15 15:06:39 +0800

    [IMP] 添加 wave 收件人详细地址处理相关测试数据

--- warehouse_wave/tests/test_express_menu.py
+++ warehouse_wave/tests/test_express_menu.py
@@ -37,3 +37,42 @@ class TestExpressMenu(TransactionCase):
         ''' 测试 get_moves_html_package '''
         move = self.delivery.sell_move_id
         self.env['wh.move'].get_moves_html_package(move.id)
+
+    def test_receiver_detail_address(self):
+        ''' Test: receiver detail address  '''
+        partner = self.env.ref('core.jd')
+        province = self.env['country.state'].search([('name', '=', u'河北省')])
+        city = self.env['all.city'].search([('city_name', '=', u'石家庄市')])
+        county = self.env['all.county'].search([('county_name', '=', u'正定县')])
+
+        # 有联系人地址child_ids
+        partner.write({'child_ids': [(0, 0, {'contact': u'开阖',
+                                             'mobile': '123',
+                                             'province_id': province.id,
+                                             'city_id': city.id,
+                                             'county_id': county.id,
+                                             'town': u'曹路镇',
+                                             'detail_address': u'河北省石家庄市正定县曹路镇金海路1688号',
+                                            })]
+                       })
+        partner._compute_partner_address()
+
+        self.order.sell_order_draft()
+        self.order.onchange_partner_id()
+        self.order.sell_order_done()
+        delivery = self.env['sell.delivery'].search([('order_id', '=', self.order.id)])
+        delivery.sell_move_id.get_receiver_goods_message()
+
+        self.order.sell_order_draft()
+        partner.child_ids[0].detail_address = '上海市浦东新区曹路镇金海路2588'
+        self.order.onchange_partner_id()
+        self.order.sell_order_done()
+        delivery_1 = self.env['sell.delivery'].search([('order_id', '=', self.order.id)])
+        delivery_1.sell_move_id.get_receiver_goods_message()
+
+        self.order.sell_order_draft()
+        partner.child_ids[0].detail_address = '内蒙古自治区阿拉善盟阿拉善左旗巴彦浩特镇阿盟一中'
+        self.order.onchange_partner_id()
+        self.order.sell_order_done()
+        delivery_1 = self.env['sell.delivery'].search([('order_id', '=', self.order.id)])
+        delivery_1.sell_move_id.get_receiver_goods_message()
