484d9a2c146038a834bda14fc497ef6d56ad8147
flora <flora@osbzr.com>
2018-07-12 10:08:57 +0800

    [IMP]goods_code 模块修改网址

--- goods_code/__manifest__.py
+++ goods_code/__manifest__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2018 上海开阖软件 ((http:www.osbzr.com).)
+# Copyright 2018 上海开阖软件 ((http://www.osbzr.com).)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
